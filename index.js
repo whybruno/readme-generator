8ab8d3a7ff45cd2c889e26c309e0fc29d1d134dc
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-02-03 16:58:33 +0000

    Add licenses Obj for index.js

--- index.js
+++ index.js
@@ -2,3 +2,23 @@
 const inquirer = require('inquirer');
 // Imports the 'fs' module for file manipulation.
 const fs = require('fs');
+
+// License information with badge URLs
+const licenses = {
+  'MIT': {
+    badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    notice: 'This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).',
+  },
+  'Apache-2.0': {
+    badge: '[![License: Apache-2.0](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    notice: 'This project is licensed under the [Apache-2.0 License](https://opensource.org/licenses/Apache-2.0).',
+  },
+  'GPL-3.0': {
+    badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+    notice: 'This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).',
+  },
+  'Other': {
+    badge: '',
+    notice: '',
+  },
+};
